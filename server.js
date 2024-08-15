e06484b367f0d8a9e4910cf67be97c8a969c5fc3
Mauricio86-make <Mauricio86-make>
2024-08-15 18:25:34 -0300

    actualized code

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
-app.get('/usuarios', (req, res) => {
+
+app.get('/usuarios', async (req, res) => {
+
+    const users = await prisma.user.findMany()
 
 
     res.status(200).json(users)
 })
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
 
-    users.push(req.body)
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
 
-    res.status(201).json({ message: "Usuário criado com sucesso"})
+    res.status(201).json(user)
 })
 
 app.listen(3000)
