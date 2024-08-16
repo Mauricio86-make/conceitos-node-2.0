5bd2fb0b4dab7daa1cd81e600b6afc18b7277da0
Mauricio86-make <Mauricio86-make>
2024-08-16 16:06:03 -0300

    actualizaed

--- server.js
+++ server.js
@@ -28,4 +28,33 @@ app.post('/usuarios', async (req, res) => {
     res.status(201).json(user)
 })
 
+app.put('/usuarios/:id', async (req, res) => {
+
+    
+
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+
+    res.status(201).json(user)
+})
+
+// app.delete('/usuarios/:id', async (req, res) => {
+//     await prisma.user.delete({
+//         where: {
+//             id: req.params.id,
+//         }
+//     })
+
+//     res.status(200).json({ message: 'Usuario deletado com sucesso!' })
+
+// })
+
 app.listen(3000)
